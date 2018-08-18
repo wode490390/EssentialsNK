13803edecfc6d096a81e37ecee9c309e528e6f02
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-08-19 00:22:14 +0100

    Add language support, more sanity checks and plugin.yml permission Debug message removed. Closes #3

--- src/main/java/cn/yescallop/essentialsnk/command/defaults/RenameCommand.java
+++ src/main/java/cn/yescallop/essentialsnk/command/defaults/RenameCommand.java
@@ -2,8 +2,11 @@ package cn.yescallop.essentialsnk.command.defaults;
 
 import cn.nukkit.Player;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.data.CommandParamType;
+import cn.nukkit.command.data.CommandParameter;
 import cn.nukkit.item.Item;
 import cn.yescallop.essentialsnk.EssentialsAPI;
+import cn.yescallop.essentialsnk.Language;
 import cn.yescallop.essentialsnk.command.CommandBase;
 
 import java.util.StringJoiner;
@@ -14,32 +17,35 @@ public class RenameCommand extends CommandBase {
     public RenameCommand(EssentialsAPI api) {
         super("rename", api);
         this.setAliases(new String[]{"renameme"});
+
+        this.commandParameters.put("default", new CommandParameter[] {
+                new CommandParameter("name", CommandParamType.STRING, false)
+        });
     }
 
     public boolean execute(CommandSender sender, String label, String[] args) {
-        if (!this.testPermission(sender)) {
+        if (!this.testPermission(sender) && !this.testIngame(sender)) {
             return false;
         }
 
-        if (sender instanceof Player){
+        Player player = (Player) sender;
 
-            Player player = (Player) sender;
-            Item item = player.getInventory().getItemInHand();
+        Item item = player.getInventory().getItemInHand();
 
-            if (item != null){
+        if (item != null && !item.isNull()){
 
-                StringJoiner newname = new StringJoiner(" ");
+            StringJoiner newName = new StringJoiner(" ");
 
-                for (String arg : args){
-                    newname.add(arg);
-                }
+            for (String arg : args){
+                newName.add(arg);
+            }
 
-                if (newname.length() < 50){
-                    item.setCustomName(newname.toString());
-                    player.getInventory().setItemInHand(item);
-                    sender.sendMessage("debug2");
-
-                }
+            if (newName.length() < 50){
+                item.setCustomName(newName.toString());
+                player.getInventory().setItemInHand(item);
+            } else {
+                Language.translate("commands.rename.length");
+                return false;
             }
         }
         return true;
