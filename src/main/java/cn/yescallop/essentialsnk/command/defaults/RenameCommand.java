244ba2c89e1f4313a292a4112ee182ce16c87428
Will <williameaster@hotmail.com>
2018-08-19 00:00:54 +0100

    Rename Command
    Item renaming command

---
+++ src/main/java/cn/yescallop/essentialsnk/command/defaults/RenameCommand.java
@@ -0,0 +1,47 @@
+package cn.yescallop.essentialsnk.command.defaults;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.item.Item;
+import cn.yescallop.essentialsnk.EssentialsAPI;
+import cn.yescallop.essentialsnk.command.CommandBase;
+
+import java.util.StringJoiner;
+
+
+public class RenameCommand extends CommandBase {
+
+    public RenameCommand(EssentialsAPI api) {
+        super("rename", api);
+        this.setAliases(new String[]{"renameme"});
+    }
+
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if (!this.testPermission(sender)) {
+            return false;
+        }
+
+        if (sender instanceof Player){
+
+            Player player = (Player) sender;
+            Item item = player.getInventory().getItemInHand();
+
+            if (item != null){
+
+                StringJoiner newname = new StringJoiner(" ");
+
+                for (String arg : args){
+                    newname.add(arg);
+                }
+
+                if (newname.length() < 50){
+                    item.setCustomName(newname.toString());
+                    player.getInventory().setItemInHand(item);
+                    sender.sendMessage("debug2");
+
+                }
+            }
+        }
+        return true;
+    }
+}
